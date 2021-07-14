080fb3ec117e1bf772dd108d74372e812f40bb81
Thomas-Radulesco <tom.radulesco@gmail.com>
2021-07-14 15:38:55 +0200

    member crud

---
+++ src/Form/MemberType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Member;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MemberType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('login')
+            ->add('password')
+            ->add('lastName')
+            ->add('firstName')
+            ->add('emailAddress')
+            ->add('gender')
+            ->add('roles')
+            ->add('createdAt')
+            ->add('updatedAt')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Member::class,
+        ]);
+    }
+}
